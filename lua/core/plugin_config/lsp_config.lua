f9ddfd92fa8662c2bc54ce8b448037051e4286dd
SturmFahrt <StormFart@proton.me>
2023-05-25 11:59:15 -0400

    Quick Change to LSP config
    just adding keybindings to lsp config.

--- lua/core/plugin_config/lsp_config.lua
+++ lua/core/plugin_config/lsp_config.lua
@@ -1,5 +1,12 @@
 require("mason").setup()
 require("mason-lspconfig").setup()
 
-require("lspconfig").lua_ls.setup {}
-require("lspconfig").rust_analyzer.setup {}
+require("lspconfig").lua_ls.setup {
+	on_attach = on_attach
+}
+
+local on_attach = function(_, _)
+	vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
+	vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
+
+end
